ffcdb5584f31f0369d2410c49fb9bd49b459bd78
hugosantanadev <132933040+hugosantanadev@users.noreply.github.com>
2025-03-11 22:00:18 -0300

    Initial commit

---
+++ src/main/java/com/app/baixatube/BaixatubeApplication.java
@@ -0,0 +1,13 @@
+package com.app.baixatube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaixatubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaixatubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/baixatube/BaixatubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.baixatube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaixatubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
